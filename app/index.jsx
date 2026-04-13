75c43523c3a2049d447cf3894d5ff6400e13140e
AR123456 <coloradoz99@gmail.com>
2026-04-13 06:24:24 -0400

    adding to view

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,15 @@
 // home page of app
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>The number 1</Text>
+      <Text>Reading List App</Text>
     </View>
   );
 };
 
 export default Home;
-
+// style sheet object
 const styles = StyleSheet.create({});
