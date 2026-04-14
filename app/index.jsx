ad05cffc7f807b16a7777ecce94b51fd87c8c461
AR123456 <coloradoz99@gmail.com>
2026-04-14 06:25:57 -0400

    making a card

--- app/index.jsx
+++ app/index.jsx
@@ -6,6 +6,9 @@ const Home = () => {
     <View style={styles.container}>
       <Text style={styles.title}>The number 1</Text>
       <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+      <View style={styles.card}>
+        <Text>Making a card</Text>
+      </View>
     </View>
   );
 };
@@ -21,5 +24,11 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: "bold",
     fontSize: 18,
+  },
+  card: {
+    backgroundColor: "#eee",
+    padding: 20,
+    borderRadius: 5,
+    boxShadow: "4px 4px rgba(0,0,0,0.1)",
   },
 });
