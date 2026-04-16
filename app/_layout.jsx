a18c1d6b78bc182d401c323874173da61f3a51a8
AR123456 <coloradoz99@gmail.com>
2026-04-16 05:30:59 -0400

    looking at stack

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,7 +2,12 @@ import { StyleSheet, Text, View } from "react-native";
 import { Slot, Stack } from "expo-router";
 
 const RootLayout = () => {
-  return <Stack></Stack>;
+  return (
+    <Stack>
+      {/* can register each screen here to give them options */}
+      <Stack.Screen name="index"></Stack.Screen>
+    </Stack>
+  );
 };
 
 export default RootLayout;
