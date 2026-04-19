ea482acda231f9f374d4fd70aba66206aebc57f4
AR123456 <coloradoz99@gmail.com>
2026-04-19 04:57:37 -0400

    theme colors const

--- components/ThemedView.jsx
+++ components/ThemedView.jsx
@@ -3,6 +3,9 @@ import { View, useColorScheme } from "react-native";
 import { Colors } from "../constants/Colors";
 
 const ThemedView = () => {
+  const colorScheme = useColorScheme();
+  // get themed object
+  const theme = Colors[colorScheme] ?? Colors.light;
   return (
     <View>
       <Text>ThemedView</Text>
