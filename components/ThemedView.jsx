deb5d53605bb8c7fbbae516ef433aa6795e6e02d
AR123456 <coloradoz99@gmail.com>
2026-04-19 04:58:19 -0400

    themed viw

--- components/ThemedView.jsx
+++ components/ThemedView.jsx
@@ -1,14 +1,17 @@
 // custom light dark component
 import { View, useColorScheme } from "react-native";
 import { Colors } from "../constants/Colors";
-// pass in destructured props
-const ThemedView = ({ style }) => {
+// pass in destructured props and any additional with props with spread operator
+const ThemedView = ({ style, ...props }) => {
   const colorScheme = useColorScheme();
   // get themed object
   const theme = Colors[colorScheme] ?? Colors.light;
   return (
     // return array of styles
-    <View styles={[{ backgroundColor: theme.background }, style]}></View>
+    <View
+      styles={[{ backgroundColor: theme.background }, style]}
+      {...props}
+    ></View>
   );
 };
 
