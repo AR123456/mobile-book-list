cb43abe2c0abd1fb81fc7f791aef0e69aeceb98e
AR123456 <coloradoz99@gmail.com>
2026-04-19 04:57:13 -0400

    boiler plate themed view

--- components/ThemedView.jsx
+++ components/ThemedView.jsx
@@ -1,1 +1,13 @@
 // custom light dark component
+import { View, useColorScheme } from "react-native";
+import { Colors } from "../constants/Colors";
+
+const ThemedView = () => {
+  return (
+    <View>
+      <Text>ThemedView</Text>
+    </View>
+  );
+};
+
+export default ThemedView;
