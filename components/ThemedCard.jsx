51753520dd1ad85a4cb48cb4142a5f4a56bae2b2
AR123456 <coloradoz99@gmail.com>
2026-04-20 04:52:25 -0400

    finishing up themed card

--- components/ThemedCard.jsx
+++ components/ThemedCard.jsx
@@ -5,12 +5,18 @@ const ThemedCard = ({ style, ...props }) => {
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme] ?? Colors.light;
   return (
-    <View style={[{ backgroundColor: theme.backgroundColor }]}>
-      <Text>ThemedCard</Text>
-    </View>
+    <View
+      style={[{ backgroundColor: theme.backgroundColor }, style]}
+      {...props}
+    />
   );
 };
 
 export default ThemedCard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 5,
+    padding: 20,
+  },
+});
