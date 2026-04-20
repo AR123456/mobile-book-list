6c31c9fb5bb2186cdd7a57ea86c37f75e3c084d0
AR123456 <coloradoz99@gmail.com>
2026-04-20 04:51:19 -0400

    centered moving on to themed card

---
+++ components/ThemedCard.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, useColorScheme, View } from "react-native";
+import { Colors } from "../constants/Colors";
+
+const ThemedCard = () => {
+  return (
+    <View>
+      <Text>ThemedCard</Text>
+    </View>
+  );
+};
+
+export default ThemedCard;
+
+const styles = StyleSheet.create({});
