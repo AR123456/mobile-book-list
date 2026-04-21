6cd32c63d99fb9f1997c97cdcf52bdb3749a6e08
AR123456 <coloradoz99@gmail.com>
2026-04-21 05:55:08 -0400

    setting up spacer component

---
+++ components/Spacer.jsx
@@ -0,0 +1,12 @@
+import { View, useColorScheme } from "react-native";
+import { Colors } from "../constants/Colors";
+
+const Spacer = () => {
+  return (
+    <View>
+      <Text>Spacer</Text>
+    </View>
+  );
+};
+
+export default Spacer;
