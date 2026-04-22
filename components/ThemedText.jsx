4cb217bb2a99d370962d2180f833230fe9ff5587
AR123456 <coloradoz99@gmail.com>
2026-04-22 06:44:01 -0400

    finish themed text with some notes

--- components/ThemedText.jsx
+++ components/ThemedText.jsx
@@ -1,10 +1,12 @@
 import { View, useColorScheme } from "react-native";
 import { Color } from "../constants/Colors";
-
-const ThemedText = () => {
+//  in title property set to default of fals
+const ThemedText = ({ style, title = false, ...props }) => {
+  // use hook to get users color scheme
   const colorScheme = useColorScheme();
+  // return the theme based on users color scheme
   const theme = Colors[colorScheme] ?? Colors.light;
-  //
+  // if true return title color from theme if false use theme text color
   const textColor = title ? theme.title : theme.text;
   return <Text style={[{ color: textColor }, style]} {...props} />;
 };
