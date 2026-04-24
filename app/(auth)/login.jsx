d68f10fda2eb60cfa2d4877ed973242b1d65da4e
AR123456 <coloradoz99@gmail.com>
2026-04-24 04:59:59 -0400

    imports for login

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,5 +1,9 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Link } from "expo-router";
+// themed components
+import ThemedView from "../../components/ThemedView";
+import ThemedText from "../../components/ThemedText";
+import Spacer from "../../components/Spacer";
 
 const login = () => {
   return (
