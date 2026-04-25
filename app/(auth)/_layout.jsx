b3d3e7fd64602fca0f24bd6c19bab59db076fa65
AR123456 <coloradoz99@gmail.com>
2026-04-25 05:29:20 -0400

    setting up a layout file for auth folder

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
+
+export default function AuthLayout() {
+  return (
+    <View>
+      <Text>AuthLayout</Text>
+    </View>
+  );
+}
