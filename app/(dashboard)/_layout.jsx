b5fbaa5d1b9011997a78002aadd298d96985aab7
AR123456 <coloradoz99@gmail.com>
2026-04-29 05:42:39 -0400

    background color tab in nav bar

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -1,10 +1,22 @@
 import { Tabs } from "expo-router";
 import { useColorScheme } from "react-native";
+import { Colors } from "../../constants/Colors";
 
 const DashboardLayout = () => {
   const colorScheme = useColorScheme();
-  const theme = Colors[colorScheme];
-  return <Tabs screenOptions={{ headerShown: false }} />;
+  const theme = Colors[colorScheme] ?? Colors.light;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: theme.navBackground,
+          paddingTop: 10,
+          height: 90,
+        },
+      }}
+    />
+  );
 };
 
 export default DashboardLayout;
