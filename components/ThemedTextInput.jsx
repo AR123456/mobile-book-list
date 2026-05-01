945c1ca1e0ac3cb1583a8c850afd44011dd9b1ab
AR123456 <coloradoz99@gmail.com>
2026-05-01 05:32:29 -0400

    themed text input

---
+++ components/ThemedTextInput.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ThemedTextInput = () => {
+  return (
+    <View>
+      <Text>ThemedTextInput</Text>
+    </View>
+  );
+};
+
+export default ThemedTextInput;
