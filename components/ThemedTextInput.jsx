b8dcf1014bc62c9ec643bd059d10a4e373723fee
AR123456 <coloradoz99@gmail.com>
2026-05-01 05:32:48 -0400

    input component

--- components/ThemedTextInput.jsx
+++ components/ThemedTextInput.jsx
@@ -1,11 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { TextInput, useColorScheme, Text, View } from "react-native";
+import { Colors } from "../constants/Colors";
 
-const ThemedTextInput = () => {
+const ThemedTextInput = ({ style, ...props }) => {
+  const colorScheme = (useColorScheme = useColorScheme());
+  const theme = Colors[colorScheme] ?? Colors.light;
   return (
-    <View>
+    <TextInput
+      style={[
+        {
+          backgroundColor: theme.uiBackground,
+          color: theme.text,
+          padding: 20,
+          borderRadius: 6,
+        },
+      ]}
+    >
       <Text>ThemedTextInput</Text>
-    </View>
+    </TextInput>
   );
 };
 
