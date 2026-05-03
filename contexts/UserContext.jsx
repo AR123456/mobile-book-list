a1a406bd9a8579060ba2cd80d2c956bf3cee3d01
AR123456 <coloradoz99@gmail.com>
2026-05-03 06:14:59 -0400

    use context hook

--- contexts/UserContext.jsx
+++ contexts/UserContext.jsx
@@ -1,1 +1,9 @@
 import { createContext } from "react";
+
+export const UserContext = createContext();
+
+// provider component to track state for the context
+export function UserProvider({ children }) {
+  // user state
+  // functions to update or mutate that state
+}
