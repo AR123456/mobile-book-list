450b48bf9e0de1f4c754e9df4ce54e9279cf0a3a
AR123456 <coloradoz99@gmail.com>
2026-05-03 06:15:17 -0400

    userContext boiler plate

--- contexts/UserContext.jsx
+++ contexts/UserContext.jsx
@@ -1,9 +1,19 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
 // provider component to track state for the context
 export function UserProvider({ children }) {
-  // user state
+  // user getter setter
+  const [user, setUser] = useState();
   // functions to update or mutate that state
+  async function login(email, password) {}
+  async function register(email, password) {}
+  async function logout() {}
+  //   return the user context and the built in provider context( which supplies values)
+  return (
+    <UserContext.Provider value={{ user, login, register, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
 }
