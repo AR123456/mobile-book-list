2aef7350993c648046a2140772364d6abca82e45
AR123456 <coloradoz99@gmail.com>
2026-05-04 06:01:21 -0400

    login  and register with notes context

--- contexts/UserContext.jsx
+++ contexts/UserContext.jsx
@@ -10,11 +10,25 @@ export function UserProvider({ children }) {
   // user getter setter
   const [user, setUser] = useState(null);
   // functions to update or mutate that state
-  async function login(email, password) {}
+  async function login(email, password) {
+    try {
+      // create session
+      await account.createEmailPasswordSession(email, password);
+      // get the created session from server
+      const response = await account.get();
+      // setter
+      setUser(response);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   // getting email and password form the input fields on the forms
   async function register(email, password) {
     try {
+      // creating, note it does not automatically log user in
       await account.create(ID.unique(), email, password);
+      // log the newly created user in
+      await login(email, password);
     } catch (error) {
       console.log(error.message);
     }
