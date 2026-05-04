600526ff498b6775f5d6323f048b2cee5ce81cfd
AR123456 <coloradoz99@gmail.com>
2026-05-04 05:59:50 -0400

    useUser hook

--- hooks/useUser.jsx
+++ hooks/useUser.jsx
@@ -1,1 +1,12 @@
 // custom hook to supply values where needed
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
+
+export function useUser() {
+  const context = useContext(UserContext);
+  //   check context null
+  if (!context) {
+    throw new Error("useUser must be used within a UserProvider");
+  }
+  return context;
+}
