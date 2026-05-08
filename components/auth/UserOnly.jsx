28096b62db363261e82ca5c72f09a31661edf92c
AR123456 <coloradoz99@gmail.com>
2026-05-08 14:28:42 -0400

    router user expo

--- components/auth/UserOnly.jsx
+++ components/auth/UserOnly.jsx
@@ -1,8 +1,11 @@
+import { useRouter } from "expo-router";
 import { useUser } from "../../hooks/useUser";
 
 // destructure children to use in template
 const UserOnly = ({ children }) => {
   const { user, authChecked } = useUser();
+  // getting this hook for appwrite expo router package - can redirect
+  const router = useRouter();
 };
 
 export default UserOnly;
