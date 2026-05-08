b4e4e9216fe9233b594d4dbbe590fbe89fad50da
AR123456 <coloradoz99@gmail.com>
2026-05-08 14:28:14 -0400

    getting user into passed into auth component

--- components/auth/UserOnly.jsx
+++ components/auth/UserOnly.jsx
@@ -1,4 +1,8 @@
+import { useUser } from "../../hooks/useUser";
+
 // destructure children to use in template
-const UserOnly = ({ children }) => {};
+const UserOnly = ({ children }) => {
+  const { user, authChecked } = useUser();
+};
 
 export default UserOnly;
