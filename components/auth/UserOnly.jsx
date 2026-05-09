66a115e367b209ee8881218cdc3411e478acc57a
AR123456 <coloradoz99@gmail.com>
2026-05-09 06:26:13 -0400

    check if still loading and return children

--- components/auth/UserOnly.jsx
+++ components/auth/UserOnly.jsx
@@ -7,11 +7,16 @@ const UserOnly = ({ children }) => {
   // getting this hook for appwrite expo router package - can redirect
   const router = useRouter();
   useEffect(() => {
-    if (authChecked && user == null) {
+    if (authChecked && user === null) {
       // no user so re direct, replace removes prior screen from history stack so users back button will not navigate back
       router.replace("./login");
     }
   }, [user, authChecked]);
+  // add condition still waiting
+  if (!authChecked || !user) {
+    return <Text>Loading</Text>;
+  }
+  return children;
 };
 
 export default UserOnly;
