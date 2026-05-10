a787e322b55f13f0ecca0abf5465757c230aee5a
AR123456 <coloradoz99@gmail.com>
2026-05-10 06:03:53 -0400

    imports for guest only

--- components/auth/GuestOnly.jsx
+++ components/auth/GuestOnly.jsx
@@ -1,1 +1,5 @@
 // similar to User ONly
+import { useRouter } from "expo-router";
+import { useUser } from "../../hooks/useUser";
+import { Text } from "react-native";
+import { useEffect } from "react";
