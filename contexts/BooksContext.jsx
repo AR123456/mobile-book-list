4b693859c084f0939ba4bc46535b259a5d896676
AR123456 <coloradoz99@gmail.com>
2026-05-12 05:36:06 -0400

    boiler for CRUD

--- contexts/BooksContext.jsx
+++ contexts/BooksContext.jsx
@@ -2,4 +2,11 @@ import { createContext, useState } from "react";
 
 export const BooksContext = createContext();
 
-export function BooksProvider({}) {}
+export function BooksProvider({ children }) {
+  const [books, setBooks] = useState;
+  async function fetchBooks() {}
+  async function fetchBookById() {}
+  async function createBook() {}
+  async function deleteBook() {}
+  async function updateBook() {}
+}
