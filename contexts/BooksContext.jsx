ab611ca4d63a8bad253b7232d347afe921c1fbb6
AR123456 <coloradoz99@gmail.com>
2026-05-12 05:36:35 -0400

    adding try catches boiler

--- contexts/BooksContext.jsx
+++ contexts/BooksContext.jsx
@@ -1,12 +1,37 @@
 import { createContext, useState } from "react";
-
+import { DATABASE_ID, TABLE_ID } from "../lib/appwrite";
 export const BooksContext = createContext();
 
 export function BooksProvider({ children }) {
-  const [books, setBooks] = useState;
-  async function fetchBooks() {}
-  async function fetchBookById() {}
-  async function createBook() {}
-  async function deleteBook() {}
-  async function updateBook() {}
+  const [books, setBooks] = useState([]);
+  async function fetchBooks() {
+    try {
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  async function fetchBookById() {
+    try {
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  async function createBook() {
+    try {
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  async function deleteBook() {
+    try {
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  async function updateBook() {
+    try {
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
 }
