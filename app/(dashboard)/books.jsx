640ab2540d8e1df9f738f8fbdb98ab546519be1e
AR123456 <coloradoz99@gmail.com>
2026-05-15 06:32:17 -0400

    flat list

--- app/(dashboard)/books.jsx
+++ app/(dashboard)/books.jsx
@@ -8,12 +8,15 @@ import ThemedView from "../../components/ThemedView";
 import ThemedCard from "../../components/ThemedCard";
 
 const Books = () => {
+  const { books } = useBooks();
   return (
     <ThemedView style={styles.container} safe={true}>
       <Spacer />
       <ThemedText title={true} style={styles.heading}>
         Reading list
       </ThemedText>
+      <Spacer />
+      <FlatList data={books} />
     </ThemedView>
   );
 };
