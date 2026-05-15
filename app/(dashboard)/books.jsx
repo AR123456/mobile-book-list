8e70ce8e867517508772c07c50751734e250ca37
AR123456 <coloradoz99@gmail.com>
2026-05-15 06:32:01 -0400

    imports for the books display

--- app/(dashboard)/books.jsx
+++ app/(dashboard)/books.jsx
@@ -1,15 +1,18 @@
-import { StyleSheet } from "react-native";
-import Spacer from "../../components/Spacer";
-import ThemedView from "../../components/ThemedView";
-import ThemedText from "../../components/ThemedText";
+import { StyleSheet, FlatList, Pressable } from "react-native";
 import { useBooks } from "../../hooks/useBooks";
-const Books = ({ children }) => {
+import { Colors } from "../../constants/Colors";
+
+import Spacer from "../../components/Spacer";
+import ThemedText from "../../components/ThemedText";
+import ThemedView from "../../components/ThemedView";
+import ThemedCard from "../../components/ThemedCard";
+
+const Books = () => {
   return (
     <ThemedView style={styles.container} safe={true}>
       <Spacer />
       <ThemedText title={true} style={styles.heading}>
         Reading list
-        {response.documents}
       </ThemedText>
     </ThemedView>
   );
