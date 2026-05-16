b4e25a867eb03d8e78477c24929d0afe8278bc90
AR123456 <coloradoz99@gmail.com>
2026-05-16 07:19:47 -0400

    correct syntax for style

--- app/(dashboard)/books.jsx
+++ app/(dashboard)/books.jsx
@@ -38,12 +38,28 @@ export default Books;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: "center",
     alignItems: "stretch",
   },
   heading: {
     fontWeight: "bold",
     fontSize: 18,
     textAlign: "center",
+  },
+  list: {
+    marginTop: 40,
+  },
+  card: {
+    width: "90%",
+    marginHorizontal: "5%",
+    marginVertical: 10,
+    padding: 10,
+    paddingLeft: 14,
+    borderLeftColor: Colors.primary,
+    borderLeftWidth: 4,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
   },
 });
