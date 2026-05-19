fa759326cb9591e0b1c7e900354e03797b75e390
AR123456 <coloradoz99@gmail.com>
2026-05-19 05:35:47 -0400

    new id file for details

---
+++ app/(dashboard)/books/[id].jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const [id] = () => {
+  return (
+    <View>
+      <Text>[id]</Text>
+    </View>
+  );
+};
+
+export default [id];
+
+const styles = StyleSheet.create({});
