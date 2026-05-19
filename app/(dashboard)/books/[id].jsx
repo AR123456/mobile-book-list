fc7bdd7ed79458070c4ffbc29392ac92f629f995
AR123456 <coloradoz99@gmail.com>
2026-05-19 05:37:15 -0400

    local search params

--- app/(dashboard)/books/[id].jsx
+++ app/(dashboard)/books/[id].jsx
@@ -1,12 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
-
-import Spacer from "../../../components/Spacer";
+// to get  dynamic part of a route -expo hook
+import { useLocalSearchParams } from "expo-router";
+// themed components
 import ThemedText from "../../../components/ThemedText";
 import ThemedView from "../../../components/ThemedView";
 import ThemedCard from "../../../components/ThemedCard";
 import ThemedButton from "../../../components/ThemedButton";
 
 const BookDetails = () => {
+  // id route params avalible - getting id from what is in [] in file name
+  const { id } = useLocalSearchParams();
   return (
     // this safe just keeps the view from being too far up the screen
     <ThemedView safe={true} style={styles.container}>
